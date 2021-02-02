efbb5857f591eb75bc43616aa391eebb06c6201c
subfor <alex.kudelko@gmail.com>
2021-02-02 22:34:03 +0200

    dz flask sqlite3

--- app.py
+++ app.py
@@ -8,6 +8,34 @@ app = Flask(__name__, template_folder='template')
 DATABASE = 'database.db'
 
 
+class Currency:
+    def __init__(self, cur_to_exchange, amount):
+        self._cur_to_exchange = cur_to_exchange
+        self._amount = amount
+        self.result = self._get_cur_exchange()
+
+    def _get_cur_rate(self):
+        url = "https://api.exchangeratesapi.io/latest"
+        response = requests.get(url)
+        rate = response.json()
+        return rate["rates"][self._cur_to_exchange]
+
+    def _get_cur_exchange(self):
+        return {"currency": self._cur_to_exchange,
+                "rate": self._get_cur_rate(),
+                "amount": self._amount,
+                "result": round(self._get_cur_rate() * float(self._amount), 4)
+                }
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -33,35 +61,6 @@ def write_to_db(cur_exchange: dict):
         """, (cur_exchange["currency"], cur_exchange["rate"], cur_exchange["amount"], cur_exchange["result"]))
 
     conn.commit()
-    return None
-
-
-class Currency:
-    def __init__(self, cur_to_exchange, amount):
-        self._cur_to_exchange = cur_to_exchange
-        self._amount = amount
-        self.result = self.get_cur_exchange()
-
-    def _get_cur_rate(self):
-        url = "https://api.exchangeratesapi.io/latest"
-        response = requests.get(url)
-        rate = response.json()
-        return rate["rates"][self._cur_to_exchange]
-
-    def get_cur_exchange(self):
-        return {"currency": self._cur_to_exchange,
-                "rate": self._get_cur_rate(),
-                "amount": self._amount,
-                "result": round(self._get_cur_rate() * float(self._amount), 4)
-                }
-
-
-def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
 
 
 @app.route('/')
@@ -76,19 +75,82 @@ def get_route(route_id, amount):
         abort(404)
     else:
         cur_to_exchanche = Currency(route_id[7:].upper(), amount)
-        result_exch = cur_to_exchanche.get_cur_exchange()
-        write_to_db(result_exch)
-        print(result_exch)
-    return render_template('result.html', exchange_result=result_exch)
+        write_to_db(cur_to_exchanche.result)
+    return render_template('result.html', exchange_result=cur_to_exchanche.result)
 
 
 @app.route('/history')
 def read_history():
-    with open("history.txt", 'r') as file:
-        lines = file.readlines()
-    return render_template('history_templ.html', exchange_history=lines)
+    result = ""
+    connection = get_db()
+    cursor = connection.cursor()
+    resp = cursor.execute("""
+                select exchange.currency_to, exchange.exchange_rate, exchange.amount, exchange.result
+                from exchange
+            """)
+    resp = resp.fetchall()
+    for row in resp:
+        result += f"<li>{row['currency_to']}, {row['exchange_rate']} ," \
+                  f" {row['amount']}, {row['result']}</li> \n"
+    return result
+
+
+@app.route('/history/currency/<cur>')
+def get_history_by_currency(cur):
+    if cur not in ("usd", "gbp", "php"):
+        abort(404)
+    else:
+        result = ""
+        connection = get_db()
+        cursor = connection.cursor()
+        resp = cursor.execute(f"""
+                    select exchange.currency_to, exchange.exchange_rate, exchange.amount, exchange.result
+                    from exchange
+                    where exchange.currency_to like ?
+                """, (cur.upper(),))
+        resp = resp.fetchall()
+        for row in resp:
+            result += f"<li>{row['currency_to']}, {row['exchange_rate']} ," \
+                      f" {row['amount']}, {row['result']}</li> \n"
+    return result
+
+
+@app.route('/history/amount_gte/<float:amount>')
+@app.route('/history/amount_gte/<int:amount>')
+def get_history_by_amount(amount):
+    result = ""
+    connection = get_db()
+    cursor = connection.cursor()
+    resp = cursor.execute(f"""
+                    select exchange.currency_to, exchange.exchange_rate, exchange.amount, exchange.result
+                    from exchange
+                    where exchange.amount >= ?
+                """, (amount,))
+    resp = resp.fetchall()
+    for row in resp:
+        result += f"<li>{row['currency_to']}, {row['exchange_rate']} ," \
+                  f" {row['amount']}, {row['result']}</li> \n"
+    return result
+
+
+@app.route('/history/statistic')
+def get_statistic():
+    result = ""
+    connection = get_db()
+    cursor = connection.cursor()
+    resp = cursor.execute(f"""
+                        select exchange.currency_to , 
+                        count(exchange.currency_to) as count_exchange,
+                        sum(exchange.result) as sum_result
+                        from exchange
+                        group by exchange.currency_to
+                    """)
+    resp = resp.fetchall()
+    for row in resp:
+        result += f'<li>{row["currency_to"]},{row["count_exchange"]}, {row["sum_result"]}\n'
+    return result
 
 
 if __name__ == '__main__':
-    init_db()
+    # init_db()
     app.run(debug=True)
